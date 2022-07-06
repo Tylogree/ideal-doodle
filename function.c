a90b4c62618bf41a7a96f141d6167638275f62e9
Tylogree <88044753+Tylogree@users.noreply.github.com>
2022-07-06 14:49:11 -0300

    Create function.c

---
+++ function.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int x;
+int a;
+int b;
+
+// y = ax + b
+
+void functiona(){
+  for(int i = 0; i <= 1; i++){
+    if(i == 0){
+       printf("\n\nA >> "); scanf("%d", &a);
+        } 
+    else if(i == 1){
+        printf("\n\nX >> "); scanf("%d", &b);
+        }
+    }
+}
+
+void mathe(a; b; x; int range;){
+    for(int i; i <= range; i++){
+        int y = a * i + b;
+        printf("\n%d << ", y);
+}
+
+int main(){
+  functiona();
+  
+
+  return 0;
+}
+  
